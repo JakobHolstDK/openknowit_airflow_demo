d200ced0657295d5e60f35c4f974a14ae663649f
Jakob Holst <jho@miracle.dk>
2023-07-04 11:40:36 +0200

    demo

--- reset.sh
+++ reset.sh
@@ -7,3 +7,19 @@ source ~/.bashrc
 export AIRFLOW_UID=$(id -u)
 docker-compose up
 
+
+if [[ $AIRFLOW_USER == "" ]];
+then
+	echo "AIRFLOW_USER not set"
+	exit 1
+fi
+
+if [[ $AIRFLOW_PASSWORD == "" ]];
+then
+	echo "AIRFLOW_PASSWORD not set"
+	exit 1
+fi
+
+docker ps |grep webserver|awk '{ print $1 }' |xargs -i{} docker exec -i {} airflow users create --username  -e kalm@openknowit.com -f kalm -l openknowit --role Admin --password 'mlak1162'
+
+
