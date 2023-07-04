1fa5e528205972f98f2df7e206ed67fb333111c2
Jakob Holst <jho@miracle.dk>
2023-07-04 11:46:11 +0200

    test

--- reset.sh
+++ reset.sh
@@ -20,6 +20,25 @@ then
 	exit 1
 fi
 
-docker ps |grep webserver|awk '{ print $1 }' |xargs -i{} docker exec -i {} airflow users create --username  -e kalm@openknowit.com -f kalm -l openknowit --role Admin --password 'mlak1162'
+if [[ $AIRFLOW_EMAIL == "" ]];
+then
+	echo "AIRFLOW_EMAIL not set"
+	AIRFLOW_EMAIL="aiflow@example.com"
+fi
+
+if [[ $AIRFLOW_FIRSTNAME == "" ]];
+then
+	echo "AIRFLOW_FIRSTNAME not  set"
+	AIRFLOW_FIRSTNAME="airflow"
+fi
+
+if [[ $AIRFLOW_LASTNAME == "" ]];
+then
+	echo "AIRFLOW_LASTNAME not  set"
+	AIRFLOW_LASTNAME="airflow"
+fi
+
+docker ps |grep webserver|awk '{ print $1 }' |xargs -i{} docker exec -i {} airflow users create --username $AIRFLOW_USER -e $AIRFLOW_EMAIL -f AIRFLOW_FIRSTNAME -l${LASTNAME}  --role Admin --password ${AIRFLOW_PASSWORD}
+
 
 
